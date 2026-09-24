88d18a9dd9d62a101813ef83be5ff4b5a436a063
hudaysawalha-web <hudasawalha3@gmail.com>
2026-09-24 22:21:26 +0300

    feat: update HomeNavbar and styles for improved layout and design consistency

--- src/components/Navbar/HomeNavbar/HomeNavbar.jsx
+++ src/components/Navbar/HomeNavbar/HomeNavbar.jsx
@@ -3,23 +3,26 @@ import "./HomwNavbar.css";
 export default function HomeNavbar() {
   return (
     <div className="Homenavbar">
-      <div className="site-logo">
-        <div className="brain-icon">
-          <i className="fa-solid fa-brain"></i>
+      <div className="Homenavbar-container">
+       
+        <div className="site-logo">
+          <div className="brain-icon">
+            <i className="fa-solid fa-brain"></i>
+          </div>
+          <h1 className="logo-title">Early Step</h1>
         </div>
-        <h1> Early Step</h1>
-      </div>
 
-      <div className="navbar-links">
-        <a href="./pages/auth/login.html" className="login-link">
-          تسجيل الدخول
-        </a>
+        {/* Action Links */}
+        <div className="navbar-links">
+          <a href="./pages/auth/login.html" className="login-link">
+            تسجيل الدخول
+          </a>
 
-        <a href="./pages/auth/registers.html" className="startnow-button">
-          <i className="fa-solid fa-wand-magic-sparkles"></i> 
-          <p>ابدأ الآن</p>
-         
-        </a>
+          <a href="./pages/auth/registers.html" className="startnow-button">
+           <i className="fa-solid fa-star"></i>
+            <span>ابدأ الآن</span>
+          </a>
+        </div>
       </div>
     </div>
   );
